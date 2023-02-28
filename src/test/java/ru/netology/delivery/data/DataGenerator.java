29abfa8fcdba3ca90beaa7d85007c0a78f9b5ca0
sanyaminkin <sanyaminkin@gmail.com>
2023-02-28 16:11:00 +0300

    DataGenerator.java modified

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,39 +1,58 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
+import lombok.Data;
 import lombok.Value;
-import lombok.val;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Random;
 
+@Data
+
 public class DataGenerator {
     private DataGenerator() {
     }
 
     public static String generateDate(int shift) {
+        LocalDate localDate = LocalDate.now();
+        LocalDate date = localDate.plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String deliveryDate = date.format(formatter);
+
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
-        return date;
+        return deliveryDate;
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
+    public static String generateValidCity() {
+        String[] citiesList = {"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск",
+                "Якутск", "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита", "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Владивосток", "Ставрополь", "Хабаровск",
+                "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга", "Кузбасс", "Кемерово", "Киров", "Кострома", "Курган", "Курск", "Санкт-Петербург",
+                "Гатчина", "Липецк", "Магадан", "Москва", "Красногорск", "Мурманск", "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск",
+                "Екатеринбург", "Смоленск", "Тамбов", "Тверь", "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
+        Random random = new Random();
+        String city = citiesList[random.nextInt(citiesList.length)];
+        return city;
+    }
+
+    public static String generateInvalidCity() {
+        String[] citiesList = {"Вашингтон", "", "12345", "Moscow"};
+        Random random = new Random();
+        String city = citiesList[random.nextInt(citiesList.length)];
         return city;
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
         return name;
     }
 
-    public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
+    public static String generatePhone() {
+        Faker faker = new Faker();
+        String phone = faker.numerify("+7##########");
         return phone;
     }
 
@@ -42,9 +61,10 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
-            return user;
+            String city = generateValidCity();
+            String name = generateName(locale);
+            String phone = generatePhone();
+            return new UserInfo(city, name, phone);
         }
     }
 
